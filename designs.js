95bd333dfc040a8643ee5590fa601d271c10538c
Elina Tomsone <elinatomsone@gmail.com>
2018-04-15 22:06:06 +0300

    makeGrid function added to create grid based on user input

--- designs.js
+++ designs.js
@@ -1,19 +1,36 @@
 // Select color input
-// Select size input
 
-const submitButton = document.getElementById('submit');
+const grid = document.getElementById("pixelCanvas");
+const submitButton = document.getElementById("submit");
 const height = document.getElementById("inputHeight");
 const width = document.getElementById("inputWidth");
+const color = document.getElementById("colorPicker");
 
 
+// function to create grid based on user input
 function makeGrid() {
-    const grid = document.getElementById("pixelCanvas");
-    const row = grid.insertRow(0);
-    const column = row.insertCell(1);
+    
+// For testing only
+    console.log(height.value, width.value, color.value);
+    
+// Remove previous grid
+    while(grid.hasChildNodes()){
+        grid.removeChild(grid.firstChild);
+    };
+
+// Create grid based on height and with input
+    for (let rows = 0; rows < height.value; rows++){
+        let tr = grid.insertRow();
+        for (let columns = 0; columns < width.value; columns++){
+            let td = tr.insertCell();
+        }
+    }
 }
 
+// call function makeGrid when submit button pressed
 submitButton.addEventListener("click", function(evt) {
+    // Prevent default action of reloading
     evt.preventDefault();
-    console.log("submit button clicked");
+    //Call makeGrid function
     makeGrid();
 });
